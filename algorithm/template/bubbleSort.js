e3d1945d9ebd66ec2917e365528ef8b450941d9a
swhana <swhan0428@gmail.com>
2024-05-14 00:53:14 +0900

    Fix Bubble Sort code

--- algorithm/template/bubbleSort.js
+++ algorithm/template/bubbleSort.js
@@ -8,9 +8,9 @@ function swap(obj, idx1, idx2) {
 }
 
 function bubbleSort(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] > array[j]) swap(array, i, j);
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (array[j] > array[j + 1]) swap(array, i, j);
     }
   }
 
